17e47f2db836f1e41ad92ed324eb92c78d419fa2
Rehan Rasool <rehan.rasool9@gmail.com>
2018-11-14 20:57:48 -0500

    Revert "deploy on heroku"
    This reverts commit b3d3c109e1cb3e86d2839b7a802ea9ea8b429e93.

--- icd9-master/test.py
+++
@@ -1,9 +0,0 @@
-from icd9 import ICD9
-
-# feel free to replace with your path to the json file
-tree = ICD9('codes.json')
-
-# list of top level codes (e.g., '001-139', ...)
-toplevelnodes = tree.children
-toplevelcodes = [node.code for node in toplevelnodes]
-print '\n'.join(toplevelcodes)
