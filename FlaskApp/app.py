93e708aa03921315c1e54c81f49c822b3d368cb5
Rehan Rasool <rehan.rasool9@gmail.com>
2018-11-15 03:33:21 -0500

    use SQLAlchemy for database stuff as its cleaner

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, String, MetaData, Integer
+# from flaskext.mysql import MySQL
 
 # DATABASE_URL = "postgres://kbsvpvqhjkrrlo:4d168ed12be5e40e5578ed832857932faa8ba341b9a94fd19fa5426e5da1578d@ec2-23-23-153-145.compute-1.amazonaws.com:5432/d6b9rdf9dvnreh"
 
@@ -8,79 +11,72 @@ import psycopg2
 import json
 
 DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-# db = SQLAlchemy(app)
-
-# # Create our database model
-# class User(db.Model):
-#     __tablename__ = "users"
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column(db.String(120), unique=True)
-
-#     def __init__(self, email):
-#         self.email = email
-
-#     def __repr__(self):
-#         return '<E-mail %r>' % self.email
-
-# # Set "homepage" to index.html
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# # Save e-mail to database and send to success page
-# @app.route('/prereg', methods=['POST'])
-# def prereg():
-#     email = None
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         # Check that email does not already exist (not a great query, but works)
-#         if not db.session.query(User).filter(User.email == email).count():
-#             reg = User(email)
-#             db.session.add(reg)
-#             db.session.commit()
-#             return render_template('success.html')
-#     return render_template('index.html')
-
-# if __name__ == '__main__':
-#     app.debug = True
-#     app.run()
-
-from flask import Flask, render_template, redirect
-# from flaskext.mysql import MySQL
-from flask import request
-from flask import jsonify
-
 app = Flask(__name__)
+db = create_engine(DATABASE_URL)
+meta = MetaData(db)
+conn = db.connect()
 
+# result = db.execute("SELECT * from Persons ")
+# rows = result.fetchall()
+# print(rows)
+
+phenotypes_table = Table('phenotypes', meta,  
+							Column('id', Integer),
+							Column('title', String),
+							Column('description', String),
+							Column('icd9_inclusion', String),
+							Column('icd9_exclusion', String),
+							Column('icd10_inclusion', String),
+							Column('icd10_exclusion', String),
+							Column('lab_id', Integer),
+							Column('medications', String),
+							Column('vital_signs_id', Integer),
+							Column('reference', String))
+
+
+# # Create
+# insert_statement = film_table.insert().values(title="Doctor Strange", director="Scott Derrickson", year="2016")
+# conn.execute(insert_statement)
+
+# Read
+select_statement = phenotypes_table.select()
+result_set = conn.execute(select_statement)
+for r in result_set:
+	print(r)
+
+# # Update
+# update_statement = film_table.update().where(film_table.c.year=="2016").values(title = "Some2016Film")
+# conn.execute(update_statement)
+
+# # Delete
+# delete_statement = film_table.delete().where(film_table.c.year == "2016")
+# conn.execute(delete_statement)
 
 ########################### helper functions ###########################
 
 def find_phenotype(phenotype):
 	print("looking for: %s" % phenotype)
-	cursor.execute("SELECT * FROM phenotypes;")
-	result = cursor.fetchall()
-	print(result)
-	output = json.dumps(result)
-	return output
+	select_statement = phenotypes_table.select()
+	result = []
+	# for r in conn.execute(select_statement):
+	# 	result.append(r)
+	rows = conn.execute(select_statement)
+	list_of_dicts = [{key: value for (key, value) in row.items()} for row in rows]
+	return list_of_dicts
 
 ########################################################################
-
 
 @app.route("/")
 def main():
 	return render_template('dashboard.html')
 
 @app.route('/lookup', methods=["POST"])
-def showSignUp():
+def lookup():
 	req = request.json['search']
 	print("request: %s" % req)
 	resp = find_phenotype(req)
+	print(resp)
+	# return jsonify(resp)
 	return jsonify(resp)
 
 @app.route('/add_phenotype', methods=["POST"])
