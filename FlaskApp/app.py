577406babcb9aab686d54e9b33d41e90fcf345d4
Rehan Rasool <rehan.rasool9@gmail.com>
2018-11-14 21:57:28 -0500

    App ready to deploy

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,49 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/pre-registration'
+db = SQLAlchemy(app)
+
+# # Create our database model
+# class User(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True)
+
+#     def __init__(self, email):
+#         self.email = email
+
+#     def __repr__(self):
+#         return '<E-mail %r>' % self.email
+
+# # Set "homepage" to index.html
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# # Save e-mail to database and send to success page
+# @app.route('/prereg', methods=['POST'])
+# def prereg():
+#     email = None
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         # Check that email does not already exist (not a great query, but works)
+#         if not db.session.query(User).filter(User.email == email).count():
+#             reg = User(email)
+#             db.session.add(reg)
+#             db.session.commit()
+#             return render_template('success.html')
+#     return render_template('index.html')
+
+# if __name__ == '__main__':
+#     app.debug = True
+#     app.run()
+
 from flask import Flask, render_template, redirect
 from flaskext.mysql import MySQL
 from flask import request
 from flask import jsonify
-# from flask_sqlalchemy import SQLAlchemy
 import phenotype_transfer as pt
 
 app = Flask(__name__)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python FlaskApp/app.py $PORT
