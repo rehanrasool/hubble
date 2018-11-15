02f06f74533ba373c38274a6bf6161f7eca606ac
Rehan Rasool <rehan.rasool9@gmail.com>
2018-11-14 22:21:03 -0500

    remove numpy and pandas dependency for now

--- FlaskApp/phenotype_transfer.py
+++ FlaskApp/phenotype_transfer.py
@@ -6,16 +6,16 @@ import os
 
 def phenotype_transfer(file, outputName=None):
     data = {}
-    fileName= file.columns[1]
-    data[fileName] = {}
-    for i in range(len(file.values)):
-        keyName = file.values[i][0]
-        values = []
-        iterate = 1
-        while iterate < len(file.columns.values) and not pd.isnull(file.iloc[i,iterate]):
-            values.append(file.values[i][iterate])
-            iterate+=1
-        data[fileName][keyName] = values
+    # fileName= file.columns[1]
+    # data[fileName] = {}
+    # for i in range(len(file.values)):
+    #     keyName = file.values[i][0]
+    #     values = []
+    #     iterate = 1
+    #     while iterate < len(file.columns.values) and not pd.isnull(file.iloc[i,iterate]):
+    #         values.append(file.values[i][iterate])
+    #         iterate+=1
+    #     data[fileName][keyName] = values
     # with open(outputName, 'w') as outfile:
     # return json.dump(data, outfile)
     return json.dumps(data)
@@ -27,14 +27,14 @@ def phenotype_transfer(file, outputName=None):
 
 def find_phenotype(phenotype):
     print("looking for: %s" % phenotype)
-    path = "phenotypes/"
-    files = os.listdir(path)
+    # path = "phenotypes/"
+    # files = os.listdir(path)
     output = []
-    for file in files:
-        read_file = pd.read_csv(path + file)
-        result = phenotype_transfer(read_file)
-        if (phenotype in result):
-            output.append(result)
+    # for file in files:
+    #     read_file = pd.read_csv(path + file)
+    #     result = phenotype_transfer(read_file)
+    #     if (phenotype in result):
+    #         output.append(result)
     return output
 
 # find_phenotype("asthma")
